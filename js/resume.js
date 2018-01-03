b1d6ceb75379de4ae9fb10eaa4fc22a925efaa74
Keifer <lewiskeifer@yahoo.com>
2018-01-03 14:32:04 -0500

    Theme update, closer to final theme push

--- js/resume.js
+++ js/resume.js
@@ -51,14 +51,21 @@
 		$(this).css("color", "white");
 		$('#switch-lang').css("color", "white");
 		$('#switch-theme').css("color", "white");
-		$('.js-scroll-trigger').css("color", "rgba(255, 255, 255, .5)");
+		//$('.js-scroll-trigger').css("color", "rgba(255, 255, 255, .5)");
+		$('.js-scroll-trigger').removeClass("nav-link-dark").addClass("nav-link");
 	}
 	else 
 	{
 		$(this).css("color", "black");
 		$('#switch-lang').css("color", "black");
 		$('#switch-theme').css("color", "black");
-		$('.js-scroll-trigger').css("color", "black");
+		//$('.js-scroll-trigger').css("color", "black");
+		
+		//$('.nav-link:hover').css("border-color", "#000 #000 #000");
+
+		//$('.js-scroll-trigger').removeClass("nav-tabs").addClass("nav-tabs-dark");
+		$('.js-scroll-trigger').removeClass("nav-link").addClass("nav-link-dark");
+		//$('.js-scroll-trigger').removeClass("nav-link:hover").addClass("nav-link-dark:hover");
 	}
   });
 
